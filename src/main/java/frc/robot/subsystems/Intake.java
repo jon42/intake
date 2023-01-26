a6e328137d393ad3a1eca6e1403f38baaa813290
Jon wick <jonw3821@outlook.com>
2023-01-25 17:05:27 -0800

    first commit

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase{
+    
+    public Intake(DoubleSolenoid mrSolenoid, ){
+
+    }
+}
